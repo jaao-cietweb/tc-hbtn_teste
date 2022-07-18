c227fc02f8c20c3dcb0e74a38af036ee6a336290
joaovg@ciandt.com <joaovg@ciandt.com>
2022-07-18 16:33:44 -0300

    Commit Introdução à Testes de software - ex0

--- 0x00/email/TesteEmail.java
+++ 0x00/email/TesteEmail.java
@@ -5,10 +5,10 @@ public class TesteEmail extends Pessoa {
     }
 
     public static boolean testar_email_sem_arroba () {
-        return emailValid("email_testedominio.com.br");
+        return !emailValid("email_testedominio.com.br");
     }
 
     public static boolean testar_email_mais_50_caracteres () {
-        return emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br");
+        return !emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br");
     }
 }
