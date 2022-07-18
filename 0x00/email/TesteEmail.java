ecce9c5fe65c3f3799283ebe1dfdd3d21d29ff97
joaovg@ciandt.com <joaovg@ciandt.com>
2022-07-18 16:31:07 -0300

    Commit Introdução à Testes de software - ex0

--- 0x00/email/TesteEmail.java
+++ 0x00/email/TesteEmail.java
@@ -1,18 +1,13 @@
-import org.junit.jupiter.api.Test;
-
 public class TesteEmail extends Pessoa {
 
-    @Test
     public static boolean testar_email_com_arroba (){
         return emailValid("email_teste@dominio.com.br");
     }
 
-    @Test
     public static boolean testar_email_sem_arroba () {
         return emailValid("email_testedominio.com.br");
     }
 
-    @Test
     public static boolean testar_email_mais_50_caracteres () {
         return emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br");
     }
